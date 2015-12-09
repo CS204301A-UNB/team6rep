94248fe41d9334e8b2c8152bb27260eaa3a9c079
smcconne <smcconne@unb.ca>
2015-12-09 01:08:14 -0400

    Incorporated Godin stuff

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -3,8 +3,10 @@ public class InfoApp {
 	public static void main(String[] args){
 		
 		InfoPrintMcConnell mcPrint = new InfoPrintMcConnell();
-		
+		InfoPrintGodin goPrint = new InfoPrintGodin();
 		System.out.println("Team 6 Member List");
+		
 		mcPrint.printMcConnell();
+		goPrint.printGodin();
 	}
 }
