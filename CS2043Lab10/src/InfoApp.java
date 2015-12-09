89dc30a7548c0f69799fdb6b3b79b5b964accfe9
smcconne <smcconne@unb.ca>
2015-12-08 23:51:23 -0400

    Added class that prints my name, modified InfoApp to call associated method

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -1,6 +1,10 @@
 
 public class InfoApp {
 	public static void main(String[] args){
+		
+		InfoPrintMcConnell mcPrint = new InfoPrintMcConnell();
+		
 		System.out.println("Team 6 Member List");
+		mcPrint.printMcConnell();
 	}
 }
